b32b9fe6df81391ffb161dab16f9f20ce37e7c7c
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-12-19 01:00:17 +0200

    fix: change eslint config import to default

--- packages/zod/eslint.config.js
+++ packages/zod/eslint.config.js
@@ -1,4 +1,4 @@
-import { config as baseConfig } from '@envyper/eslint-config/base'
+import { config as baseConfig } from '@envyper/eslint-config'
 
 /** @type {import("eslint").Linter.Config} */
 export default [...baseConfig]
