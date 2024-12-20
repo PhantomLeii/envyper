87de82dd144256e40e419572cbe24cbed12fef06
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-12-20 08:48:19 +0200

    chore: add route chaining

--- apps/api/src/routes/projects.ts
+++ apps/api/src/routes/projects.ts
@@ -1,35 +1,35 @@
 import { Hono } from "hono";
 
-const projects = new Hono();
+const projects = new Hono()
 
-projects.get("/", (c) => {
-  c.status(200);
-  return c.json({ body: [], status: 200 });
-});
+  .get("/", (c) => {
+    c.status(200);
+    return c.json({ body: [], status: 200 });
+  })
 
-projects.get("/:id{[0-9]+}", (c) => {
-  return c.json({
-    data: [],
-    message: `Returns a single project with ID: ${c.req.param("id")}`,
+  .get("/:id{[0-9]+}", (c) => {
+    return c.json({
+      data: [],
+      message: `Returns a single project with ID: ${c.req.param("id")}`,
+    });
+  })
+
+  .post("/", (c) => {
+    return c.json({ data: [], message: "Creates a new project" });
+  })
+
+  .patch("/:id{[0-9]+}", (c) => {
+    return c.json({
+      data: [],
+      message: `Updates a project with ID: ${c.req.param("id")}`,
+    });
+  })
+
+  .delete("/:id{[0-9]+}", (c) => {
+    return c.json({
+      data: [],
+      message: `Deletes a project with ID: ${c.req.param("id")}`,
+    });
   });
-});
-
-projects.post("/", (c) => {
-  return c.json({ data: [], message: "Creates a new project" });
-});
-
-projects.patch("/:id{[0-9]+}", (c) => {
-  return c.json({
-    data: [],
-    message: `Updates a project with ID: ${c.req.param("id")}`,
-  });
-});
-
-projects.delete("/:id{[0-9]+}", (c) => {
-  return c.json({
-    data: [],
-    message: `Deletes a project with ID: ${c.req.param("id")}`,
-  });
-});
 
 export default projects;
