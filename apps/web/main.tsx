cf984da835afeb91aa6ece737f186fc30e3bc289
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-12-21 08:32:13 +0200

    chore: add base router config files

--- apps/web/main.tsx
+++ apps/web/main.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/globals.css";
 
-function App() {
+const App = () => {
   return (
     <>
-      <h1 className="text-6xl font-bold w-full text-center tracking-wide">
-        Hello World
-      </h1>
-      <p className="text-xl tracking-tight">Welcome to your React app</p>
+      <div className="h-screen flex items-center justify-center w-full">
+        <h1 className="text-6xl font-bold tracking-wide text-center">
+          App.tsx works
+        </h1>
+        <p className="text-xl tracking-tight">Entry Point</p>
+      </div>
     </>
   );
-}
+};
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import path from "path";
+
+console.log(path.resolve(__dirname, "./"));
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    TanStackRouterVite({
+      generatedRouteTree: "./routeTree.gen.ts",
+      routesDirectory: "./app",
+    }),
+    react(),
+  ],
   server: {
     port: 3002,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./"),
+    },
   },
 });
