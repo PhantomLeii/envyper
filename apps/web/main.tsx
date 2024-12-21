f202811371c1b72ff31c39359cc2c88c17d37f7f
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-12-21 07:55:30 +0200

    chore: add base tailwind configs

--- apps/web/main.tsx
+++ apps/web/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/globals.css";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
-      <p>Welcome to your React app</p>
+      <h1 className="text-6xl font-bold w-full text-center tracking-wide">
+        Hello World
+      </h1>
+      <p className="text-xl tracking-tight">Welcome to your React app</p>
     </>
   );
 }
