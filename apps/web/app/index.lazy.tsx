1af78032ecd863c993d5b5a4a8863bf13ec4e415
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-12-23 10:08:13 +0200

    chore: add next ui component lib

--- apps/web/app/index.lazy.tsx
+++ apps/web/app/index.lazy.tsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { Button } from "@nextui-org/button";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
@@ -8,6 +9,9 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button onPress={() => console.log("Hello from Index.tsx Button")}>
+        Press Me!
+      </Button>
     </div>
   );
 }
--- apps/web/tailwind.config.cjs
+++ apps/web/tailwind.config.cjs
@@ -1,13 +1,16 @@
 const theme = require("@envyper/tailwind-config/tailwind.config");
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./index.html",
     "./*.tsx",
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
+    "../../node_modules/@nextui-org/theme/dist/*.{js,jsx,ts,tsx}",
   ],
   theme,
-  plugins: [],
+  plugins: [nextui()],
 };
