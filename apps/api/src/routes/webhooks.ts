072194a144cf9ce3b1c6d661b93ec7379c0de24f
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-12-24 11:30:06 +0200

    feat: add webhook verification with svix

--- apps/api/src/routes/webhooks.ts
+++ apps/api/src/routes/webhooks.ts
@@ -1,8 +1,63 @@
 import { Hono } from "hono";
+import { Webhook } from "svix";
 
 const webhooks = new Hono().post("/", async (c) => {
-  console.dir(await c.req.json());
-  return c.json({ data: await c.req.json(), message: "Webhook received!" });
+  const SIGNING_SECRET = process.env.SIGNING_SECRET as string;
+  if (!SIGNING_SECRET) {
+    // send error to expection tracking service
+    throw new Error("Error: Please add SIGNING_SECRET from Clerk Dashboard");
+  }
+
+  const hook = new Webhook(SIGNING_SECRET);
+
+  const payload = await c.req.text();
+  console.log(payload);
+
+  const svixId = c.req.header("svix-id");
+  const svixTimestamp = c.req.header("svix-timestamp");
+  const svixSignature = c.req.header("svix-signature");
+
+  if (!svixId || !svixTimestamp || !svixSignature) {
+    // send error to error tracking service
+    console.log("Error: Missing svix headers");
+
+    return c.json(
+      {
+        message: "Error: Missing svix headers",
+      },
+      400,
+    );
+  }
+
+  let event: any;
+
+  try {
+    event = hook.verify(payload, {
+      "svix-id": svixId as string,
+      "svix-timestamp": svixTimestamp as string,
+      "svix-signature": svixSignature as string,
+    });
+  } catch (err) {
+    console.log("Error: Could not verify webhook:", err);
+    return c.json(
+      {
+        message: "Error: Could not verify webhook",
+      },
+      400,
+    );
+  }
+
+  const { id } = event.data;
+  const eventType = event.type;
+  console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
+  console.log("Webhook payload:", event.data);
+
+  return c.json(
+    {
+      message: "Webhook received",
+    },
+    200,
+  );
 });
 
 export default webhooks;
