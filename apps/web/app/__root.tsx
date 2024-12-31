bfcd972c649f4b3c3534282452ea56df3de614b4
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2024-12-31 18:48:26 +0200

    chore: add query client provider

--- apps/web/app/__root.tsx
+++ apps/web/app/__root.tsx
@@ -1,9 +1,11 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { Component as Navbar } from "@/components/NavbarMenu";
 import { NextUIProvider } from "@nextui-org/react";
-import type { NavigateOptions, ToOptions } from "@tanstack/react-router";
 import { useRouter } from "@tanstack/react-router";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import type { NavigateOptions, ToOptions } from "@tanstack/react-router";
 
 declare module "@react-types/shared" {
   interface RouterConfig {
@@ -16,6 +18,8 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string;
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing clerk publishable key");
 }
+
+const client = new QueryClient();
 
 export const Route = createRootRoute({
   component: RootLayout,
@@ -31,15 +35,17 @@ function RootLayout() {
       signInUrl="/sign-in"
       signUpUrl="/sign-up"
     >
-      <NextUIProvider
-        navigate={(to, options) => router.navigate({ to, ...options })}
-        useHref={(to) => router.buildLocation({ to }).href}
-      >
-        <Navbar />
-        <div className="container mx-auto relative top-[64px] w-full py-2">
-          <Outlet />
-        </div>
-      </NextUIProvider>
+      <QueryClientProvider client={client}>
+        <NextUIProvider
+          navigate={(to, options) => router.navigate({ to, ...options })}
+          useHref={(to) => router.buildLocation({ to }).href}
+        >
+          <Navbar />
+          <div className="container mx-auto relative top-[64px] w-full py-2">
+            <Outlet />
+          </div>
+        </NextUIProvider>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 }
