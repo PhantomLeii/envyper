d09d9f3c9b4ab876c1d6d203b5628403f937d044
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-01-16 21:14:27 +0200

    feat: add tanstack router integration

--- apps/web/src/context/Providers.tsx
+++ apps/web/src/context/Providers.tsx
@@ -1,9 +1,27 @@
 import { NextUIProvider } from "@nextui-org/react";
+import type { NavigateOptions, ToOptions } from "@tanstack/react-router";
+import { useRouter } from "@tanstack/react-router";
+
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    href: ToOptions["to"];
+    routerOptions: Omit<NavigateOptions, keyof ToOptions>;
+  }
+}
 
 type ProviderProps = {
   children: React.ReactNode;
 };
 
 export default function Providers(props: ProviderProps) {
-  return <NextUIProvider>{props.children}</NextUIProvider>;
+  const router = useRouter();
+
+  return (
+    <NextUIProvider
+      navigate={(to, options) => router.navigate({ to, ...options })}
+      useHref={(to) => router.buildLocation({ to }).href}
+    >
+      {props.children}
+    </NextUIProvider>
+  );
 }
