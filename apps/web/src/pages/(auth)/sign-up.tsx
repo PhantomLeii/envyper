8d4b83434864a488dcf5c56989226098215150bc
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-01-17 19:54:33 +0200

    add sign up form

--- apps/web/src/pages/(auth)/sign-up.tsx
+++ apps/web/src/pages/(auth)/sign-up.tsx
@@ -1,9 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { SignUp } from "@clerk/clerk-react";
 
-export const Route = createFileRoute('/(auth)/sign-up')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/(auth)/sign-up")({
+  component: SignUpForm,
+});
 
-function RouteComponent() {
-  return <div>Hello "/(auth)/sign-up"!</div>
+function SignUpForm() {
+  return (
+    <div className="h-[calc(100vh-6px)] grid place-items-center w-full">
+      <SignUp />
+    </div>
+  );
 }
