6ba698d22b69840099150b69b515d5fdde0bb01f
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-01-24 15:55:04 +0200

    feat: configure routing

--- apps/web/src/context/Providers.tsx
+++ apps/web/src/context/Providers.tsx
@@ -1,9 +1,27 @@
 import { HeroUIProvider } from "@heroui/react";
+import type { NavigateOptions, ToOptions } from "@tanstack/react-router";
+import { useRouter } from "@tanstack/react-router";
+
+declare module "@react-types/shared" {
+  interface RouterConfig {
+    href: ToOptions["to"];
+    routerOptions: Omit<NavigateOptions, keyof ToOptions>;
+  }
+}
 
 type ProviderProps = {
   children: React.ReactNode;
 };
 
 export default function Providers(props: ProviderProps) {
-  return <HeroUIProvider>{props.children}</HeroUIProvider>;
+  const router = useRouter();
+
+  return (
+    <HeroUIProvider
+      navigate={(to, options) => router.navigate({ to, ...options })}
+      useHref={(to) => router.buildLocation({ to }).href}
+    >
+      {props.children}
+    </HeroUIProvider>
+  );
 }
