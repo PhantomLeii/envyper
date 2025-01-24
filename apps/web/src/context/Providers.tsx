2ec7d2fd5e0865cfcfd54dea0ab6307e1c546ba1
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-01-24 18:32:25 +0200

    Feat: User authentication (#24)
    * update navbar route links
    
    * feat: add auth form
    
    * feat: add auth context
    
    * update providers
    
    * feat: add basic dashboard
    
    * update project deps

--- apps/web/src/context/Providers.tsx
+++ apps/web/src/context/Providers.tsx
@@ -1,6 +1,7 @@
 import { HeroUIProvider } from "@heroui/react";
 import type { NavigateOptions, ToOptions } from "@tanstack/react-router";
 import { useRouter } from "@tanstack/react-router";
+import { AuthProvider } from "./Authentication";
 
 declare module "@react-types/shared" {
   interface RouterConfig {
@@ -17,11 +18,15 @@ export default function Providers(props: ProviderProps) {
   const router = useRouter();
 
   return (
-    <HeroUIProvider
-      navigate={(to, options) => router.navigate({ to, ...options })}
-      useHref={(to) => router.buildLocation({ to }).href}
-    >
-      {props.children}
-    </HeroUIProvider>
+    <>
+      <AuthProvider>
+        <HeroUIProvider
+          navigate={(to, options) => router.navigate({ to, ...options })}
+          useHref={(to) => router.buildLocation({ to }).href}
+        >
+          {props.children}
+        </HeroUIProvider>
+      </AuthProvider>
+    </>
   );
 }
