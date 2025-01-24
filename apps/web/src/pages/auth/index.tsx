f5d514ca576a6ecb193ba8001bdaf678000ed444
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-01-24 18:03:06 +0200

    feat: add auth form

--- apps/web/src/pages/auth/index.tsx
+++ apps/web/src/pages/auth/index.tsx
@@ -1,9 +1,43 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useCallback, useEffect, useMemo } from "react";
+import { Hanko, register } from "@teamhanko/hanko-elements";
+import { useRouter } from "@tanstack/react-router";
+
+const hankoApi = import.meta.env.VITE_HANKO_API_URL;
 
 export const Route = createFileRoute("/auth/")({
   component: AuthenticationForm,
 });
 
 function AuthenticationForm() {
-  return <div>Hello "/auth/"!</div>;
+  const { navigate } = useRouter();
+  const hanko = useMemo(() => new Hanko(hankoApi), []);
+
+  const redirectAfterLogin = useCallback(() => {
+    navigate({ to: "/" });
+  }, [navigate]);
+
+  useEffect(
+    () =>
+      hanko.onSessionCreated(() => {
+        redirectAfterLogin();
+      }),
+
+    [hanko, redirectAfterLogin],
+  );
+
+  useEffect(() => {
+    register(hankoApi).catch((err) => {
+      // send to exeption tracking service
+      if (import.meta.env.NODE_ENV !== "production") console.log(err);
+    });
+  }, []);
+
+  return (
+    <>
+      <main className="min-h-[calc(100vh-64px)] w-full grid place-items-center">
+        <hanko-auth />
+      </main>
+    </>
+  );
 }
