04fccf8c31ad356b1d20d500dfe0f077e66b73e0
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-01-25 22:22:59 +0200

    feat: update projects routes

--- apps/server/projects/urls.py
+++ apps/server/projects/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import ProjectDetailAPIView, ProjectOperationAPIView
 
 urlpatterns = [
-    
+    path('', ProjectDetailAPIView.as_view(), name='projects'),
+    path('<int:project_id>/', ProjectOperationAPIView.as_view(), name='project-operations'),
 ]
--- apps/server/projects/views.py
+++ apps/server/projects/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Project
+from .serializers import ProjectSerializer
+
+
+class ProjectDetailAPIView(APIView):    
+    def get(self, request):
+        projects = Project.objects.filter(creatorId='test-user')
+        return Response({"data": projects.values()}, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        project = Project.objects.create(creatorId='test-user')
+        return Response({"data": project.values()}, status=status.HTTP_201_CREATED)
+
+
+class ProjectOperationAPIView(APIView):
+    def get(self, request, project_id):
+        project = Project.objects.get(pk=project_id)
+        if not project:
+            return Response({'message': 'Project not found'},status=status.HTTP_404_NOT_FOUND)
+
+        if project.creatorId != 'test-user':
+            return Response({'message': 'Project not found'},status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ProjectSerializer(project)
+
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+    
+    # TODO: Fix attributes error bug & implement update method
+    def patch(self, request, project_id):
+        project = Project.objects.get(id=project_id)
+
+        if not (request.data.get('name') or request.data.get('description')):
+            return Response({'message': 'No provided data'}, status=status.HTTP_400_BAD_REQUEST)
+    
+        if not project:
+            return Response({'message': 'Project not found'},status=status.HTTP_404_NOT_FOUND)
+
+        if project.creatorId != 'test-user':
+            return Response({'message': 'Project not found'},status=status.HTTP_404_NOT_FOUND)
+            
+        project.objects.update(**request.data)
+        project.save()
+        return Response({"data": project.values()}, status=status.HTTP_200_OK)
+
+    def delete(self, request, project_id):
+        project = Project.objects.get(id=project_id)
+        if not project:
+            return Response({'message': 'Project not found'},status=status.HTTP_404_NOT_FOUND)
+        
+        if project.creatorId != 'test-user':
+            return Response({'message': 'Project not found'},status=status.HTTP_404_NOT_FOUND)
+        
+        project.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
