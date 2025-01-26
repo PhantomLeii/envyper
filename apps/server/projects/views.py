d9bd75a42176647b121e1a3446960924b2636f49
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-01-26 10:56:36 +0200

    Feat: Projects View & Routes (#27)
    * chore: add dockerignore
    
    * add patch view
    
    * add data validation method
    
    * feat: add create project view
    
    * chore: clean up to do comments

--- apps/server/projects/views.py
+++ apps/server/projects/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+
 from .models import Project
 from .serializers import ProjectSerializer
 
@@ -12,8 +13,12 @@ class ProjectDetailAPIView(APIView):
         return Response({"data": projects.values()}, status=status.HTTP_200_OK)
 
     def post(self, request):
-        project = Project.objects.create(creatorId='test-user')
-        return Response({"data": project.values()}, status=status.HTTP_201_CREATED)
+        serializer = ProjectSerializer(data={"creatorId": "test-user", **request.data})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
+        
+        return Response({"message": 'Provided data is invalid'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ProjectOperationAPIView(APIView):
@@ -29,22 +34,21 @@ class ProjectOperationAPIView(APIView):
 
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
     
-    # TODO: Fix attributes error bug & implement update method
     def patch(self, request, project_id):
         project = Project.objects.get(id=project_id)
 
-        if not (request.data.get('name') or request.data.get('description')):
-            return Response({'message': 'No provided data'}, status=status.HTTP_400_BAD_REQUEST)
-    
         if not project:
             return Response({'message': 'Project not found'},status=status.HTTP_404_NOT_FOUND)
 
         if project.creatorId != 'test-user':
             return Response({'message': 'Project not found'},status=status.HTTP_404_NOT_FOUND)
-            
-        project.objects.update(**request.data)
-        project.save()
-        return Response({"data": project.values()}, status=status.HTTP_200_OK)
+
+        serializer = ProjectSerializer(project, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"data": serializer.data}, status=status.HTTP_200_OK)
+
+        return Response({"message": "Provided data is invalid"}, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, project_id):
         project = Project.objects.get(id=project_id)
