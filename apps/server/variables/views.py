abf4c2ab208dfd183a2374b035fd27ad9caf61f3
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-02-03 05:37:16 +0200

    fix: add get method

--- apps/server/variables/views.py
+++ apps/server/variables/views.py
@@ -15,21 +15,13 @@ class VariableAPIView(APIView):
         if not project_id:
             return Response({"message": "Project ID is required"}, status=status.HTTP_400_BAD_REQUEST)
         
-        project = Project.objects.get(pk=project_id)
-        if not project:
+        try:
+            project = Project.objects.get(pk=project_id)
+            if project.creatorId != 'test-user':
+                return Response({"message": "You are not authorized to view this project's variables"}, status=status.HTTP_401_UNAUTHORIZED)
+            
+            variables = Variable.objects.filter(project_id=project_id)
+            return Response({"data": variables.values()}, status=status.HTTP_200_OK)
+        except Project.DoesNotExist:
             return Response({"message": "Project not found"}, status=status.HTTP_404_NOT_FOUND)
         
-        if project.creatorId != 'test-user':
-            return Response({"message": "You are not authorized to view this project's variables"}, status=status.HTTP_401_UNAUTHORIZED)
-        
-        variables = Variable.objects.filter(project_id=project_id)
-        return Response(VariableSerializer(variables, many=True).data, status=status.HTTP_200_OK)
-
-
-    # # TODO: Create this API view
-    # def post(self, request):
-    #     serizializer = VariableSerializer(data=request.data)
-    #     if serizializer.is_valid():
-    #         serizializer.save()
-    #         return Response(serizializer.data, status=status.HTTP_201_CREATED)
-    #     return Response({"message": "Provided data is invalid"}, status=status.HTTP_400_BAD_REQUEST)
