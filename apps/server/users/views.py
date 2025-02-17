7e43b5fa69a2163fe3882db3f987766e32c33955
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-02-17 13:09:41 +0200

    feat: add user detail get handler

--- apps/server/users/views.py
+++ apps/server/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -15,3 +16,16 @@ class CreateUserAPIView(APIView):
             serializer.save()
             return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetailAPIView(APIView):
+    user_model = get_user_model()
+    
+    def get(self, request):
+        try:
+            user = self.user_model.objects.get(email=request.user.email)
+            serializer = UserSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except self.user_model.DoesNotExist:
+            return Response({"detail": "You are not authorized"}, status=status.HTTP_401_UNAUTHORIZED)
+        
