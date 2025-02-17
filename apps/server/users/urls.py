8587ad274f8297be3111e3bd1e64a49f84303a39
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-02-17 12:12:17 +0200

    add simple jwt support

--- apps/server/users/urls.py
+++ apps/server/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 from .views import CreateUserAPIView
 
 urlpatterns = [
+    path("user/token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path('user/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path("user/create/", CreateUserAPIView.as_view(), name="create-user"),
 ]
