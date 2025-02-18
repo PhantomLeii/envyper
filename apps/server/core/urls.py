9ea2233cdcdd0cbaa44b4e13888deaeeff36d095
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-02-18 14:30:29 +0200

    update projects tests

--- apps/server/core/urls.py
+++ apps/server/core/urls.py
@@ -10,5 +10,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("auth/", include("users.urls")),
+    path("projects/", include("projects.urls")),
     path("admin/", admin.site.urls),
 ]
--- apps/server/projects/tests/test_views.py
+++ apps/server/projects/tests/test_views.py
@@ -4,8 +4,10 @@ from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 from rest_framework import status
 
+from ..models import Projects
 
-class ProjectAPIViewTests(TestCase):
+
+class TestSetup(TestCase):
     def setUp(self):
         user_data = {
             "first_name": "testuser",
@@ -25,7 +27,7 @@ class ProjectAPIViewTests(TestCase):
         self.token = response.data["access"]
 
         self.valid_project_data = {
-            "creator": self.user.id,
+            "creator": self.user,
             "name": "Test Project",
         }
 
@@ -35,44 +37,57 @@ class ProjectAPIViewTests(TestCase):
             "description": "Test Description",
         }
 
-        self.update_project_data = {
-            "name": "Updated Project",
-            "description": "Updated Description",
-        }
+
+class ProjectAPIViewTests(TestSetup):
+    def test_invalid_project_data(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
+        response = self.client.post(reverse("projects"), self.invalid_project_data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_project(self):
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
-        response = self.client.post(reverse("project"), self.valid_project_data)
+        response = self.client.post(reverse("projects"), self.valid_project_data)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertContains(response.data["data"], self.valid_project_data)
         self.assertEqual(response.data["data"]["creator"], self.user.id)
 
-    def test_get_project(self):
+    def test_get_projects(self):
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
-        response = self.client.get(reverse("project-detail"))
+        response = self.client.get(reverse("projects"))
+
+        self.assertContains(response.data["data"], self.test_project)
+
+
+class ProjectDetailAPIViewTests(TestSetup):
+    def setUp(self):
+        super().setUp()
+
+        self.test_project = Projects.objects.create(**self.valid_project_data)
+
+        self.update_data = {
+            "name": "Updated Project",
+            "description": "Updated description",
+        }
+
+    def test_get_project_by_id(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
+        response = self.client.get(
+            reverse("project-detail", project_id=self.test_project.id)
+        )
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertContains(response.data["data"], self.valid_project_data)
-        self.assertEqual(response.data["data"]["creator"], self.user.id)
+        self.assertDictEqual(response.data["data"], self.test_project)
 
     def test_update_project(self):
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
         response = self.client.patch(
-            reverse("project-detail"), self.update_project_data
+            reverse("project-detail", project_id=self.test_project.id),
+            self.update_data,
         )
 
         response_data = response.data["data"]
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response_data["name"], self.update_project_data["name"])
-        self.assertEqual(
-            response_data["description"], self.update_project_data["description"]
-        )
-
-    def test_invalid_project_data(self):
-        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
-        response = self.client.post(
-            reverse("create-project"), self.invalid_project_data
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response_data["name"], self.update_data["name"])
+        self.assertEqual(response_data["description"], self.update_data["description"])
