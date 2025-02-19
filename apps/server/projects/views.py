0ff1253a9a938ef03b65219f3c4f20c977ff6070
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-02-19 19:32:38 +0200

    add variable api views

--- apps/server/projects/views.py
+++ apps/server/projects/views.py
@@ -2,8 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializers import ProjectSerializer, ProjectDetailSerializer
-from .models import Projects
+from .serializers import (
+    ProjectSerializer,
+    ProjectDetailSerializer,
+    VariableSerializer,
+    VariableDetailSerializer,
+)
+from .models import Projects, Variables
 
 
 class ProjectsAPIView(APIView):
@@ -62,19 +67,40 @@ class ProjectDetailAPIView(APIView):
 
 
 class VariablesAPIView(APIView):
-    def get(self, request):
-        pass
+    def post(self, request, project_id):
+        data = {**request.data, "project": project_id, "author": request.user.id}
+        serializer = VariableSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def post(self, request):
-        pass
+    def get(self, request, project_id):
+        project_id = request.query_params.get("project")
+        if project_id is None:
+            return Response(
+                {"detail": "Project ID is required"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if not Projects.objects.filter(pk=project_id, creator=request.user.id).exists():
+            return Response(
+                {"detail": "Project does not exist"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        variables = Variables.objects.filter(
+            author=request.user.id, project=request.query_params.get("project")
+        )
+
+        serializer = VariableSerializer(variables, many=True)
+        return Response({"data": serializer.data}, status=status.HTTP_200_OK)
 
 
 class VariableDetailAPIView(APIView):
-    def get(self, request, variable_id):
+    def get(self, request, project_id, variable_id):
         pass
 
-    def patch(self, request, variable_id):
+    def patch(self, request, project_id, variable_id):
         pass
 
-    def delete(self, request, variable_id):
+    def delete(self, request, project_id, variable_id):
         pass
