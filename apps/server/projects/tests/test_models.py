2de9f197113ff5a5db70ecf4e21f309dcfb0af3f
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-02-19 07:45:57 +0200

    chore: update projects model tests

--- apps/server/projects/tests/test_models.py
+++ apps/server/projects/tests/test_models.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from ..models import Projects
+from ..models import Projects, Variables
 
 
-class ProjectsModelTests(TestCase):
+class TestSetup(TestCase):
     def setUp(self):
         self.test_user = get_user_model().objects.create_user(
             email="testuser@email.com",
@@ -18,6 +18,11 @@ class ProjectsModelTests(TestCase):
         }
 
         self.project = Projects.objects.create(**self.project_data)
+
+
+class ProjectsModelTests(TestSetup):
+    def setUp(self):
+        super().setUp()
 
     def test_project_creation(self):
         self.assertEqual(self.project.creator, self.test_user)
@@ -36,3 +41,42 @@ class ProjectsModelTests(TestCase):
         project_data = {**self.project_data, "creator": None}
         with self.assertRaises(Exception):
             Projects.objects.create(**project_data)
+
+
+class VariablesModelTests(TestSetup):
+    def setUp(self):
+        super().setUp()
+        self.variable_data = {
+            "project": self.project,
+            "author": self.test_user,
+            "key": "test_key",
+            "value": "test_value",
+        }
+
+        self.variable = Variables.objects.create(**self.variable_data)
+
+    def test_variable_creation(self):
+        self.assertEqual(self.variable.project, self.project)
+        self.assertEqual(self.variable.author, self.test_user)
+        self.assertEqual(self.variable.key, self.variable_data["key"])
+        self.assertEqual(self.variable.value, self.variable_data["value"])
+
+    def test_key_is_required(self):
+        variable_data = {**self.variable_data, "key": ""}
+        with self.assertRaises(Exception):
+            Variables.objects.create(**variable_data)
+
+    def test_value_is_required(self):
+        variable_data = {**self.variable_data, "value": ""}
+        with self.assertRaises(Exception):
+            Variables.objects.create(**variable_data)
+
+    def test_author_is_required(self):
+        variable_data = {**self.variable_data, "author": None}
+        with self.assertRaises(Exception):
+            Variables.objects.create(**variable_data)
+
+    def test_project_is_required(self):
+        variable_data = {**self.variable_data, "project": None}
+        with self.assertRaises(Exception):
+            Variables.objects.create(**variable_data)
