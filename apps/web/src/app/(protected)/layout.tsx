d49fe0d1517d3d0d1fd40b502df40ed68e9f00e0
Leboang Phoshoko <phoshokolebogang@outlook.com>
2025-03-08 23:18:07 +0200

    update dashboard page layout card

--- apps/web/src/app/(protected)/layout.tsx
+++ apps/web/src/app/(protected)/layout.tsx
@@ -1,3 +1,4 @@
+import { Card, CardHeader } from "@heroui/card";
 import React from "react";
 
 interface AppLayoutProps {
@@ -5,7 +6,22 @@ interface AppLayoutProps {
 }
 
 const AppLayout = (props: AppLayoutProps) => {
-  return <main>{props.children}</main>;
+  return (
+    <main className="container w-full grid grid-cols-10 gap-6">
+      <Card className="col-span-3 rounded p-6 flex gap-4" fullWidth>
+        <CardHeader className="flex items-center">
+          <img
+            className="w-32 h-32 rounded-full"
+            src="https://avatars.githubusercontent.com/u/6216367?v=4"
+            alt="Avatar"
+          />
+          <h1 className="text-xl font-bold mt-4">John Doe</h1>
+          <p className="text-sm text-gray-500">Software Engineer at Company</p>
+        </CardHeader>
+      </Card>
+      {props.children}
+    </main>
+  );
 };
 
 export default AppLayout;
